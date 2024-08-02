a191869953c4ff0d26708b0b68d06ac38bb1bd69
Luckas <luckasranarison@gmail.com>
2024-08-02 14:45:34 +0300

    fix(query): javascript argument captures (#32)
    Closes #32.

--- queries/javascript/class.scm
+++ queries/javascript/class.scm
@@ -1,9 +1,11 @@
 (call_expression
   function: [
-    (identifier) @ident (#any-of? @ident "clsx" "classnames" "tw" "css")
+    (identifier) @ident
     (member_expression
-      object: (identifier) @object-ident (#eq? @object-ident "tw"))
+      object: (identifier) @object-ident)
   ]
+  (#any-of? @ident "clsx" "classnames" "tw" "css")
+  (#eq? @object-ident "tw")
   arguments: [
     (arguments
       (_)+) @tailwind ; the actual class range is extracted in the code
