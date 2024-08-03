670d32e69b9f099d4b938682983e4ddf5e7c2ab4
Brian Rivera Martinez <contactbrianriveramartinez@gmail.com>
2024-08-03 16:32:55 -0600

    Update class.scm

--- queries/typescript/class.scm
+++ queries/typescript/class.scm
@@ -1,1 +1,14 @@
-; inherits: javascript
+(call_expression
+  function: [
+    (identifier) @ident
+    (member_expression
+      object: (identifier) @object-ident)
+  ]
+  (#any-of? @ident "clsx" "classnames" "tw" "css")
+  (#eq? @object-ident "tw")
+  arguments: [
+    (arguments
+      (_)+) @tailwind ; the actual class range is extracted in the code
+    (template_string
+      (string_fragment) @tailwind)
+  ])
