d8c741c3e3032acf51fdf26dac16ccf17c95cca1
Brian Rivera Martinez <contactbrianriveramartinez@gmail.com>
2024-08-03 16:35:36 -0600

    Update class.scm

--- queries/javascript/class.scm
+++ queries/javascript/class.scm
@@ -12,3 +12,13 @@
     (template_string
       (string_fragment) @tailwind)
   ])
+
+(jsx_attribute
+  (property_identifier) @_attribute_name
+  (#any-of? @_attribute_name "class" "className" "style" "css" "tw")
+  [
+    (string
+      (string_fragment) @tailwind)
+    (jsx_expression
+      (_) @tailwind)
+  ])
