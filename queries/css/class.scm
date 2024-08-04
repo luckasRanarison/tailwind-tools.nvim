f473c848658165cf9380a72d78c6446c1c06b530
Luckas <luckasranarison@gmail.com>
2024-08-04 15:33:56 +0300

    wip: quantified capture wrapper

--- queries/css/class.scm
+++ queries/css/class.scm
@@ -1,4 +1,5 @@
-(postcss_statement
-  (at_keyword) @_keyword
-  (#eq? @_keyword "@apply")
-  (plain_value)+) @tailwind ; the actual class range is extracted in the code
+((postcss_statement
+   (at_keyword) @_keyword
+   (#eq? @_keyword "@apply")
+   (plain_value)+) @tailwind.inner ; the actual class range is extracted in the code
+ (#set! @tailwind.inner "start" 1))
