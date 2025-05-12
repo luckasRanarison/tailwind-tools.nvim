53edd1b63d2146571abeedbb288db0e5a2d98fa1
Garrett Hopper <garrett@garretthopper.com>
2025-05-12 11:42:36 -0500

    fix test

--- tests/queries/clojure_spec.lua
+++ tests/queries/clojure_spec.lua
@@ -5,11 +5,10 @@ runner.test({
   provider = "luapattern",
   file = "tests/queries/clojure/test.cljs",
   ranges = {
+    { 4, 9, 4, 14 },
     { 0, 6, 0, 11, delimiter = { pattern = "%.", raw = "." } },
     { 1, 9, 1, 14, delimiter = { pattern = "%.", raw = "." } },
     { 2, 3, 2, 8, delimiter = { pattern = "%.", raw = "." } },
-    { 2, 3, 2, 8, delimiter = { pattern = "%.", raw = "." } },
     { 3, 6, 3, 11, delimiter = { pattern = "%.", raw = "." } },
-    { 4, 9, 4, 14 },
   },
 })
