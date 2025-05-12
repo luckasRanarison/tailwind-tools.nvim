420dc0fec89ae9e1d46a120a26254608f9377eeb
Garrett Hopper <garrett@garretthopper.com>
2025-05-12 09:29:23 -0500

    add back Clojure test

---
+++ tests/queries/clojure/test.cljs
@@ -0,0 +1,5 @@
+[:div.c1.c2]
+[:div#id.c1.c2]
+[:.c1.c2] ; defaults to div
+[:#id.c1.c2]
+{:class "c1 c2"}
---
+++ tests/queries/clojure_spec.lua
@@ -0,0 +1,15 @@
+local runner = require("tests.queries.runner")
+
+runner.test({
+  name = "closure",
+  provider = "luapattern",
+  file = "tests/queries/clojure/test.cljs",
+  ranges = {
+    { 0, 6, 0, 11, delimiter = { pattern = "%.", raw = "." } },
+    { 1, 9, 1, 14, delimiter = { pattern = "%.", raw = "." } },
+    { 2, 3, 2, 8, delimiter = { pattern = "%.", raw = "." } },
+    { 2, 3, 2, 8, delimiter = { pattern = "%.", raw = "." } },
+    { 3, 6, 3, 11, delimiter = { pattern = "%.", raw = "." } },
+    { 4, 9, 4, 14 },
+  },
+})
